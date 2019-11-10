18e241c0015bfba4361d8aa286706ed3027c3ae2
jlramos97 <jlramos97@gmail.com>
2019-11-10 14:53:01 +0100

    Toqueteo vario

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './components/HomeScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <HomeScreen/>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
